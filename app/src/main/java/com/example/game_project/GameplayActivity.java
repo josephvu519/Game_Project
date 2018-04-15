8cc00ebefdeb2040061add5596870a79db7f510f
jzv5153 <josephvu519@gmail.com>
2018-04-15 19:06:26 -0400

    Created rotation for bubble

--- app/src/main/java/com/example/game_project/GameplayActivity.java
+++ app/src/main/java/com/example/game_project/GameplayActivity.java
@@ -1,12 +1,21 @@
 package com.example.game_project;
 
 import android.content.Intent;
+import android.hardware.Sensor;
+import android.hardware.SensorEvent;
+import android.hardware.SensorEventListener;
+import android.hardware.SensorManager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import android.util.DisplayMetrics;
 import android.view.Gravity;
 import android.view.View;
+import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.RelativeLayout;
+import android.widget.TextView;
 import android.widget.Toast;
 import com.example.game_project.Accessories.Timer;
 
@@ -15,9 +24,21 @@ public class GameplayActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.gameplay);
         final Timer timer = new Timer();
+
+        //TEST
+        final ImageView bubble = findViewById(R.id.bubble);
+        DisplayMetrics displayMetrics = new DisplayMetrics();
+        getWindowManager().getDefaultDisplay().getMetrics(displayMetrics);
+        final int screenHeight = displayMetrics.heightPixels;
+        final int screenWidth = displayMetrics.widthPixels;
+        final int maxTilt = 10;
+        final TextView axisX = findViewById(R.id.xAxis);
+        //-------
+
 
         Toast instruction = Toast.makeText(this, "Tilt the screen to move", Toast.LENGTH_LONG);
         instruction.setGravity(Gravity.TOP, 0, 0);
@@ -44,6 +65,35 @@ public class GameplayActivity extends AppCompatActivity {
         });*/
 
 
+        SensorManager sensorManager = (SensorManager) getSystemService(SENSOR_SERVICE);
+        Sensor rotationSensor = sensorManager.getDefaultSensor(Sensor.TYPE_GAME_ROTATION_VECTOR);
+        SensorEventListener gyroscopeEventListener = new SensorEventListener() {
+            @Override
+            public void onSensorChanged(SensorEvent sensorEvent) {
+                double axisY = sensorEvent.values[1] * 180 / Math.PI;
+                double axisZ = sensorEvent.values[2] * 180 / Math.PI;
+                RelativeLayout.LayoutParams lp = (RelativeLayout.LayoutParams) bubble.getLayoutParams();
+                if (axisY > maxTilt){
+                    lp.setMargins((screenWidth - bubble.getWidth()), (int)(screenHeight*.8), 0, 0);
+                    bubble.setLayoutParams(lp);
+                }
+                else if (axisY < -maxTilt) {
+                    lp.setMargins(0, (int)(screenHeight*.8), (screenWidth/2), 80);
+                    bubble.setLayoutParams(lp);
+                }
+                else {
+                    lp.setMargins((int)((screenWidth/2 - bubble.getWidth()/2) + axisY * (screenWidth-bubble.getWidth())/2/maxTilt), (int)(screenHeight*.8), 0, 80);
+                    bubble.setLayoutParams(lp);
+                    axisX.setText("Z Axis: " + sensorEvent.values[2] * 180 /Math.PI);
+                }
+            }
+
+            @Override
+            public void onAccuracyChanged(Sensor sensor, int i) {
+
+            }
+        };
+        sensorManager.registerListener(gyroscopeEventListener, rotationSensor, SensorManager.SENSOR_DELAY_FASTEST);
 
     }
 }
